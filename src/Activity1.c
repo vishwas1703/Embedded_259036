4c9fa4b6a7963fafe0fd4f8d96b7a5aebc529996
vishwas1703 <vishwashv3@gmail.com>
2021-04-28 18:35:27 +0530

    update5

--- src/Activity1.c
+++ src/Activity1.c
@@ -1,21 +1,51 @@
+/**
+ * @file Activity1.c
+ * @author vishwas.H ()
+ * @brief functions of activity1
+ * @version 0.1
+ * @date 2021-04-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include<avr/io.h>
+
 #include"Activity1.h"
-void activity_1(void)
+
+
+void LED_PORTB0_HIGH(void)
 {
-    
-    while(1)
-    {
-        /**
-         * @brief conditional opearator 
-         * @note if both switches are closed LED will glow, similar to AND gate
-         */
-        if(!((PIND &(1<<PIND0)) | (PIND &(1<<PIND2)) ))   
-        {
-           LED_PORTB0_HIGH();
-        }
-        else
-        {
-            LED_PORTB0_LOW();
-        }
-    }
+   PORTB |=(1<<PB0);
+}
+
+/** \brief LED connected to PORT B0 will turn low
+ *
+ *
+ */
+
+void LED_PORTB0_LOW(void)
+{
+      PORTB&=~(1<<PB0);
+
+}
+
+
+
+/**
+ * @brief Initialise of input output ports
+ * 
+ */
+void initialise_ports_a1(void)
+{
+     /*configure LED pin*/
+    DDRB|=(1<<PB0); 
+    /*PD0 as input port*/                                        
+    DDRD&=~(1<<PD0);  
+    /*PD0 internal pull up*/                                      
+    PORTD|=(1<<PD0);                                        
+   /*PD2 as input port*/   
+    DDRD&=~(1<<PD2);                                        
+     /*PD2 internal pull up*/  
+    PORTD|=(1<<PD2);                                        
 }
